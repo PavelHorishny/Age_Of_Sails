416e4f62b9650a7f68691f3774859e0232c1166b
PavelHorishny <horishnypavel@gmail.com>
2022-06-18 20:41:18 +0300

    test

--- src/main/java/org/game/Main.java
+++ src/main/java/org/game/Main.java
@@ -5,7 +5,7 @@ import org.DAO.VesselType;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
         Vessel vessel = new Vessel(VesselType.FRIGATE);
         System.out.println(vessel.isCanFire());
         System.out.println(vessel.getHitPoint());
