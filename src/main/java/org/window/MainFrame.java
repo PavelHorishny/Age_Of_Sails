3c2ee2920493099342249f731709946c8e436d3b
PavelHorishny <horishnypavel@gmail.com>
2022-06-19 23:37:07 +0300

    possible view of the frame

--- src/main/java/org/window/MainFrame.java
+++ src/main/java/org/window/MainFrame.java
@@ -1,5 +1,33 @@
 package org.window;
 
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
 
+public class MainFrame extends JFrame{
+
+    public MainFrame(){
+        JSplitPane splitPane = new JSplitPane();
+        JPanel leftPanel = new JPanel();
+        JPanel rightPanel = new JPanel();
+        JSplitPane split = new JSplitPane();
+        JPanel bottomPanel = new JPanel();
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(1000, 600));
+        getContentPane().setLayout(new GridLayout());
+        getContentPane().add(splitPane);
+        split.setEnabled(false);
+        splitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
+        splitPane.setDividerLocation(700);
+        splitPane.setLeftComponent(leftPanel);
+        splitPane.setRightComponent(split);
+        split.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        split.setDividerLocation(300);
+        split.setTopComponent(rightPanel);
+        split.setBottomComponent(bottomPanel);
+        pack();
+    }
+    public static void main(String[] args){
+        EventQueue.invokeLater(() -> new MainFrame().setVisible(true));
+    }
 }
